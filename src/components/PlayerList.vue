4aa650781b92d97959985a30113d3a88afc74128
Azamat Uzbekov <batukasss@gmail.com>
2021-11-04 01:21:57 -0600

    created list of players but names come as NaN

---
+++ src/components/PlayerList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div  v-for="(player, index) in players" :key="index">
+        <p>{{player.player-name}}</p>        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'player-list',
+        props: ['players', 'playerView']
+    }
+</script>
+
--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -1,23 +1,6 @@
 <template>
     <div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>{{ player.first_name}}</td>
-                    <td>{{ player.last_name }}</td>
-                    <td>{{ player.score }}</td>
-                </tr>
-            </tbody>
-        </table>
+
     </div>
 </template>
 
